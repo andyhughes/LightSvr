e0fe5ee2041d47bc4cc7b0080c7e2b925f32cfdc
Andy Hughes <deliveryprinciples@gmail.com>
2016-11-02 12:03:50 +0000

    removed data db/jenkins files

--- nodeserver/db_info.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "test_svr" : "mongodb://mongosvr:27017",
-  "test_db_name" : "testdb",
-  "test_collection_static" : "testData",
-  "LDF_db_name" : "LDF",
-  "LDF_collection" : "v1"
-};
